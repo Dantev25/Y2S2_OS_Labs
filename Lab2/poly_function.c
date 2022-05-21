d79fc4f87e58eb979b98862fa3630f82489d1707
Dantev25 <95751895+Dantev25@users.noreply.github.com>
2022-05-21 16:42:34 +0400

    updated lab2q1

--- Lab2/poly_function.c
+++ Lab2/poly_function.c
@@ -5,16 +5,14 @@
 void poly(int *x, int *result)
 {
 	printf("\n\nf(x): x^4 + x^3 + 3x^2 + 2");
-	printf("\nPlease input x: ");
-	scanf("%d",&x);
+	printf("\n\nPlease input x: ");
+	scanf("%d",x);
+	*result = pow(*x,4) + pow(*x,3) + (3*pow(*x,2)) + 2;
 }
 
 void main()
 {
 	int x,result;
-	printf("\n\nf(x): x^4 + x^3 + 3x^2 + 2");
-	printf("\nPlease input x: ");
-	scanf("%d",&x);
-	poly(x,&result);
-	printf("f(%d) = ",x,result);
+	poly(&x,&result);
+	printf("\nf(%d) = %d\n\n",x,result);
 }
