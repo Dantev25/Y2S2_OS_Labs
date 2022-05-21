6abdbde8d3ea4eb079e1531aa8dff184843c5c5e
Dantev25 <95751895+Dantev25@users.noreply.github.com>
2022-05-21 20:52:27 +0400

    fixed q1 and q3 of lab 2

--- Oomar/Lab2/poly_function.c
+++ Oomar/Lab2/poly_function.c
@@ -2,17 +2,19 @@
 #include <math.h>
 
 
-void poly(int *x, int *result)
+int poly(int *x)
 {
+	int result;
 	printf("\n\nf(x): x^4 + x^3 + 3x^2 + 2");
 	printf("\n\nPlease input x: ");
 	scanf("%d",x);
-	*result = pow(*x,4) + pow(*x,3) + (3*pow(*x,2)) + 2;
+	result = pow(*x,4) + pow(*x,3) + (3*pow(*x,2)) + 2;
+	return result;
 }
 
 void main()
 {
 	int x,result;
-	poly(&x,&result);
+	result = poly(&x);
 	printf("\nf(%d) = %d\n\n",x,result);
 }
--- Oomar/Lab2/q3.c
+++ Oomar/Lab2/q3.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef struct{
+	double rad;
+	double vol;
+	double area;
+}sphere;
 
-void arvolsph(double *rad, double *area, double *vol)
+sphere arvolsph(double r)
 {
-	printf("\nPlease input radius of the circle: ");
-	scanf("%lf",rad);
-	while(*rad<0){
-		printf("Please input again, radius cannot be negative\n");
-		scanf("%lf",rad);
-	}
-	printf("Radius = %.2lf",*rad);
-	*vol = pow(*rad,3)*M_PI*(4.0/3);
-	*area = pow(*rad,2)*4*M_PI;
-	printf("\nArea = %.3lf\nVolume = %.3lf\n",*area,*vol);
+	sphere a;
+	a.rad=r;
+	printf("Radius = %.2lf",a.rad);
+	a.area = pow(a.rad,3)*M_PI*(4.0/3);
+	a.vol = pow(a.rad,2)*4*M_PI;
+	printf("\nArea = %.3lf\nVolume = %.3lf\n",a.area,a.vol);
+	return a;
 }
 
 void main()
 {
-	double r1, r2, v1, v2, a1, a2,la,lv;
+	int i=0;
+	double r,la,lv;
+	sphere out, in;
 	printf("\nInner ball: \n");
-	arvolsph(&r1,&a1,&v1);
-	printf("\n\nOuter ball: \n");
-	arvolsph(&r2,&a2,&v2);
-	while (r1>r2){
-		printf("\nError- Outer ball cannot have smaller radius: Input again.\n");
-		arvolsph(&r2,&a2,&v2);
-
+	printf("\nPlease input radius of the circle: ");
+	scanf("%lf",&r);
+	while(r<0){
+		printf("Please input again, radius cannot be negative\n");
+		scanf("%lf",&r);
 	}
-	la = a1+a2;
-	lv = v2-v1;
+	in = arvolsph(r);
+	
+	printf("\n\nOuter ball: \n");
+	scanf("%lf",&r);
+	while(r<0){
+		printf("Please input again, radius cannot be negative\n");
+		scanf("%lf",&r);
+		while (in.rad>r){
+			printf("\nError- Outer ball cannot have smaller radius: Input again.\n");
+			scanf("%lf",&r);
+			out = arvolsph(r);
+		}
+	}
+	out = arvolsph(r);
+
+	la =  in.area + out.area;
+	lv = out.vol - in.vol;
 	printf("\nSurface area liquid covers = %.3lf\n",la);
 	printf("Volume of liquid contained = %.3lf\n\n",lv);
 }
